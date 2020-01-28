e401f0e57c1699bdd9bd3bb8c7321ad3b4229e7e
boriza <boriza@users.noreply.github.com>
2020-01-29 09:32:34 +1100

    initial local run

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# cosmosdb-change-feed-fn
-Event sourcing with cosmos db change feed and azure function
+# Cosmos DB change feed trigger processing with Azure Functions v2
+
+1) install azure function cli, az cli on your machine, VS.Code and Azure Functions Extension
+
+2) in local.settings.json
+```
+{
+  "IsEncrypted": false,
+  "Values": {
+    "AzureWebJobsStorage": "STORAGE CONTAINER CONNECTION",
+    "FUNCTIONS_WORKER_RUNTIME": "dotnet",
+    "DOCUMENTDB": "DOCUMENTDB CONNECTOIN STRING"
+  }
+}
+```
+3) Finally run locally func start --build
+
+4) Deploy with 
+```
+func azure functionapp publish <FunctionAppName> --publish-local-settings -i	
+
+```
---
+++ cosmosdb-change-feed-fn.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.Function
+{    public static class CosmosTrigger
+    {
+        [FunctionName("CosmosTrigger")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "ToDoList",
+            collectionName: "Items",
+            ConnectionStringSetting = "DOCUMENTDB",
+            LeaseCollectionName = "leases",
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> documents,
+            ILogger log)
+        {
+            if (documents != null && documents.Count > 0)
+            {
+                log.LogInformation($"Documents modified: {documents.Count}");
+                log.LogInformation($"First document Id: {documents[0].Id}");
+                log.LogInformation($"The document: {documents[0].ToString()}");
+            }
+        }
+    }
+}
